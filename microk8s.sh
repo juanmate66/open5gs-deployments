ec4e67cbe889600f77fa072e7e95c2172e20a00c
juanmate66 <juannmate@gmail.com>
2025-07-15 11:46:43 -0500

    Update microk8s.sh

--- microk8s.sh
+++ microk8s.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+# Instalar Microk8s, extension de kubernetes
+-	sudo snap install microk8s --classic
+# Dar permisos de uso
+-	sudo usermod -a -G microk8s admin
+-	sudo mkdir -p ~/.kube
+-	sudo chown -f -R admin ~/.kube
+# Permitir addons necesarios
+-	microk8s enable dashboard dns registry helm storage ingress
+# Instalar herramienta helm 
+-	sudo snap install helm --classic
+-	sudo usermod -a -G microk8s admin
+-	sudo chown -f -R admin ~/.kube
+-	newgrp microk8s 
+-	microk8s config > ~/.kube/config
+
+# Reiniciar VM para que se confirme los permisos
+-	sudo reboot
