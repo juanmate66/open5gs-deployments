ed92c46c003b0d7d0509c0c0a48781cd26be6c28
juanmate66 <juannmate@gmail.com>
2025-07-15 11:44:18 -0500

    Update deployment3.sh

--- deployment3.sh
+++ deployment3.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+#IMPORTANTE: TENER EL FICHERO  gnb-ues-values.yaml y modificar "count=3"
+# Desplegar Core 5G
+-	helm install open5gs oci://registry-1.docker.io/gradiantcharts/open5gs --version 2.2.9 --values https://gradiant.github.io/5g-charts/docs/open5gs-ueransim-gnb/5gSA-values.yaml
+
+# Resgistrar un nuevo usuario a traves del populate
+# Ingresar a Pod populate
+-	microk8s kubectl exec deployment/open5gs-populate -ti – bash
+# Registrar nuevo usuario
+-	open5gs-dbctl add_ue_with_slice 999700000000003 465B5CE8B199B49FAA5F0A2EE238A6BC E8ED289DEBA952E4283B54E88E6183CA internet 1 111111
+
+# Desplegar gNode con nuevo usuario 
+
+-	helm install ueransim-gnb oci://registry-1.docker.io/gradiant/ueransim-gnb --version 0.2.6 –values gnb-ues-values.yaml
